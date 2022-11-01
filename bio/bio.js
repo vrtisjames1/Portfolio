bc045b731d589ea83d0d68566c21a0a770f8eef9
James Vrtis <jamesvrtis@caskradmins-mbp.mynetworksettings.com>
2022-11-01 14:56:49 -0400

    nav links updated

---
+++ bio/bio.js
@@ -0,0 +1,24 @@
+$(() => {
+
+let timesClicked = 0;
+  const $openNav = () => {
+    timesClicked++;
+    if (timesClicked % 2 == 0){
+          $(`#navigation`).css(`height`,`30px`)
+          $(`.navButton`).css(`display`,`none`)
+    } else {
+      $(`#navigation`).css(`height`,`130px`)
+      $(`.navButton`).css(`display`,`block`)
+    }
+
+};
+
+
+  // const $closeNav = () => {
+  //   $(`#navigation`).css(`width`,`35px`);
+  //   console.log(`hello`)
+  // }
+
+  $(`.navHomeButton`).on(`click`, $openNav);
+  // $(`.navHomeButton`).on(`click`, $closeNav);
+})
