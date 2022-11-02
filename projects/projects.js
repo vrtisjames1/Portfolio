91693a958d793ef8af7f603cf99d0047c178ee57
James Vrtis <jamesvrtis@caskradmins-mbp.mynetworksettings.com>
2022-11-02 15:08:27 -0400

    dbscan update

--- projects/projects.js
+++ projects/projects.js
@@ -16,6 +16,13 @@ const $carouselRight = () => {
       $carouselImages.children().eq(rightClick-1).detach().prependTo($carouselImages)
         }
 
+const $projectText =() => {
+  const $projectDes = $(`<p>`).appendTo(`#title`);
+  if($carouselImages.children().eq(0).contains('.Pokemon')){
+    $projectDes.text(`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proiden`);
+  }
+}
+
 $('.right').on('click', $carouselRight);
 $('.left').on('click', $carouselLeft);
 
