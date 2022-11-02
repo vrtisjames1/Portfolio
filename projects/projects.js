aadccf14fd97d7718c7e6893dc48f026f35bb38e
James Vrtis <jamesvrtis@caskradmins-mbp.mynetworksettings.com>
2022-11-02 14:45:01 -0400

    carsoule completed

--- projects/projects.js
+++ projects/projects.js
@@ -1,11 +1,27 @@
 $(() => {
 
+///////////////////
+///////////////////carousel
+// let rightClick = 0
+
+const $carouselRight = () => {
+  const $carouselImages=$(`#carouselImages`);
+  let rightClick = $carouselImages.children().length;
+  $carouselImages.children().eq(0).detach().appendTo($carouselImages)
+    }
+
+    const $carouselLeft = () => {
+      const $carouselImages=$(`#carouselImages`);
+      let rightClick = $carouselImages.children().length;
+      $carouselImages.children().eq(rightClick-1).detach().prependTo($carouselImages)
+        }
+
+$('.right').on('click', $carouselRight);
+$('.left').on('click', $carouselLeft);
+
 
 ///////////////////carousel
-const $carousel = () => {
-  
-}
-
+///////////////////
 
 let timesClicked = 0;
   const $openNav = () => {
