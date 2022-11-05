ab7572ff9cf5aa3a0b2721137d8aa38c20a2ab5f
James Vrtis <jamesvrtis@caskradmins-mbp.mynetworksettings.com>
2022-11-05 06:12:29 -0400

    updated project text

--- projects/projects.js
+++ projects/projects.js
@@ -24,11 +24,11 @@ const $projectText =() => {
   const $carouselImages=$(`#carouselImages`);
 
   if($carouselImages.children().eq(0).hasClass(`pokemon`)){
-    $projectDes.text(`GOTTA CATCH EM ALL!... I developed this project with a group. This game was originally a terminal prompt game, and developed into an interactive DOM Data browser game. The player will fight multiple enemies until all of them are destroyed are until the user decides to flee!`);
+    $projectDes.text(`[DOM, Pokemon game] GOTTA CATCH EM ALL!... This game was developed with a partner. This game was originally a terminal prompt game, and transformed into an interactive DOM browser game. The player will fight multiple enemies until all of them are destroyed, or until the player decides to flee!`);
   } else if ($carouselImages.children().eq(0).hasClass(`dbscan`)) {
-    $projectDes.text(`DBSCAN... False data injection... This is an unsupervised data group graduate project developed with programming languages R and Python. The voltage data from a power line was analyzed to see how well three unsupervised methods could detect erroneous errors.  `);
+    $projectDes.text(`[DBSCAN, False data injection] This is an unsupervised data group graduate project developed with programming languages R and Python. The voltage data from a power line was analyzed to see how well three unsupervised methods could detect erroneous errors.  `);
   } else if ($carouselImages.children().eq(0).hasClass(`prompt`)) {
-    $projectDes.text(`Prompt Game...General Assembly Landscaper Classic!... This is a General Assembly classic game that was integrated with a partner through an interactive prompt terminal experience. The player character is a landscaper who needs to make choices and save enough money to win the game.`);
+    $projectDes.text(`[Prompt Game, General Assembly Landscaper Classic] This is a General Assembly game that was developed with a partner and transformed into an interactive prompt terminal experience. The player character is a landscaper who needs to make choices and save enough money and gather enough tools to win the game.`);
   }
 }
 
