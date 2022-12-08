36101bcd97b7c28a33d74f6ed597f5aa434b0479
James Vrtis <jamesvrtis@caskradmins-mbp.mynetworksettings.com>
2022-12-08 16:06:27 -0500

    updated code

--- projects/projects.js
+++ projects/projects.js
@@ -32,8 +32,10 @@ const $projectText =() => {
   }  else if ($carouselImages.children().eq(0).hasClass(`marvel`)) {
     $projectDes.text(`[Express, Arrays] This is a company website created for the marvel characters. This website used express to manage a server and ejs to manage the array data.`);
   } else if ($carouselImages.children().eq(0).hasClass(`secretsanta`)) {
-    $projectDes.text(`[Express, MongoDB] This is a secret santa website where groups can update their profiles which contains an image, wishlist, and partner restrictin list. The website will randomly assign partners and email the results in secret`);
-    $('<a>Secret Santa Website</a>').attr("href","https://ancient-bastion-28884.herokuapp.com/").appendTo($projectDes);
+    $projectDes.text(`[Express, MongoDB] This is a secret santa website where groups can update their profiles which contains an image, wishlist, and partner restrictin list. The website will randomly assign partners and email the results in secret
+    `);
+    $('<a>( Secret Santa Website )</a>').attr("href","https://ancient-bastion-28884.herokuapp.com/").appendTo($projectDes);
+    $('<a>( Secret Santa Code )</a>').attr("href","https://git.generalassemb.ly/vrtisjames/app").appendTo($projectDes);
   }
 }
 
