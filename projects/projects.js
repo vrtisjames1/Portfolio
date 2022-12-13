f53a24dc7a489472186b3753a06e56fc8f83b790
James Vrtis <jamesvrtis@caskradmins-mbp.mynetworksettings.com>
2022-12-13 12:19:52 -0500

    updated bootstrap

--- projects/projects.js
+++ projects/projects.js
@@ -4,43 +4,43 @@ $(() => {
 ///////////////////carousel
 // let rightClick = 0
 
-const $carouselRight = () => {
-  const $carouselImages=$(`#carouselImages`);
-  let rightClick = $carouselImages.children().length;
-  $carouselImages.children().eq(0).detach().appendTo($carouselImages)
-    }
+// const $carouselRight = () => {
+//   const $carouselImages=$(`#carouselImages`);
+//   let rightClick = $carouselImages.children().length;
+//   $carouselImages.children().eq(0).detach().appendTo($carouselImages)
+//     }
 
-    const $carouselLeft = () => {
-      const $carouselImages=$(`#carouselImages`);
-      let rightClick = $carouselImages.children().length;
-      $carouselImages.children().eq(rightClick-1).detach().prependTo($carouselImages)
-        }
+//     const $carouselLeft = () => {
+//       const $carouselImages=$(`#carouselImages`);
+//       let rightClick = $carouselImages.children().length;
+//       $carouselImages.children().eq(rightClick-1).detach().prependTo($carouselImages)
+//         }
 
 
-const $projectDes = $(`<p>`).attr('id',`projectDescription`).appendTo(`#projectHeader`);
+// const $projectDes = $(`<p>`).attr('id',`projectDescription`).appendTo(`#projectHeader`);
 
-const $projectText =() => {
+// const $projectText =() => {
 
-  const $carouselImages=$(`#carouselImages`);
+//   const $carouselImages=$(`#carouselImages`);
 
-  if($carouselImages.children().eq(0).hasClass(`pokemon`)){
-    $projectDes.text(`[DOM, Pokemon Game] GOTTA CATCH EM ALL!... This game was developed with a partner. The game originally was created as a terminal prompt game, but transformed into an interactive DOM browser game. The character player will fight multiple enemies until all of them are destroyed, or until the player decides to flee!`);
-  }  else if ($carouselImages.children().eq(0).hasClass(`marvel`)) {
-    $projectDes.text(`[Express, Arrays] This is a company website created for the marvel characters. This website used express to manage a server and ejs to manage the array data.`);
-  } else if ($carouselImages.children().eq(0).hasClass(`secretsanta`)) {
-    $projectDes.text(`[Express, MongoDB] This is a secret santa website where groups can update their profiles which contains an image, wishlist, and partner restrictin list. The website will randomly assign partners and email the results in secret
-    `);
-    $('<a>( Secret Santa Website )</a>').attr("href","https://ancient-bastion-28884.herokuapp.com/").appendTo($projectDes);
-    $('<a>( Secret Santa Code )</a>').attr("href","https://git.generalassemb.ly/vrtisjames/app").appendTo($projectDes);
-  }
-}
+//   if($carouselImages.children().eq(0).hasClass(`pokemon`)){
+//     $projectDes.text(`[DOM, Pokemon Game] GOTTA CATCH EM ALL!... This game was developed with a partner. The game originally was created as a terminal prompt game, but transformed into an interactive DOM browser game. The character player will fight multiple enemies until all of them are destroyed, or until the player decides to flee!`);
+//   }  else if ($carouselImages.children().eq(0).hasClass(`marvel`)) {
+//     $projectDes.text(`[Express, Arrays] This is a company website created for the marvel characters. This website used express to manage a server and ejs to manage the array data.`);
+//   } else if ($carouselImages.children().eq(0).hasClass(`secretsanta`)) {
+//     $projectDes.text(`[Express, MongoDB] This is a secret santa website where groups can update their profiles which contains an image, wishlist, and partner restrictin list. The website will randomly assign partners and email the results in secret
+//     `);
+//     $('<a>( Secret Santa Website )</a>').attr("href","https://ancient-bastion-28884.herokuapp.com/").appendTo($projectDes);
+//     $('<a>( Secret Santa Code )</a>').attr("href","https://git.generalassemb.ly/vrtisjames/app").appendTo($projectDes);
+//   }
+// }
 
-$projectText();
+// $projectText();
 
-$('.right').on('click', $carouselRight);
-$('.left').on('click', $carouselLeft);
-$('.right').on('click', $projectText);
-$('.left').on('click', $projectText);
+// $('.right').on('click', $carouselRight);
+// $('.left').on('click', $carouselLeft);
+// $('.right').on('click', $projectText);
+// $('.left').on('click', $projectText);
 
 ///////////////////carousel
 ///////////////////
